c9b99f292b142745ec887b06d591621e18025f60
joacarrilloco <joacarrilloco@unal.edu.co>
2019-09-18 23:38:03 -0500

    Dfs working for 15 movements

--- rubik.cpp
+++ rubik.cpp
@@ -1,4 +1,5 @@
 #include<iostream> 
+#include<map>
 
 using namespace std;
 
@@ -23,12 +24,13 @@ const int ROTATIONS[TOTAL_ROTATIONS][ROT_SIZE] =
 
 struct Cube
 {
-	short mat[STICKERS];
+	string mat;
 
 	Cube()
 	{
+		mat = "";
 		for( int i = 0; i < STICKERS; ++ i )
-			mat[i] = ( i / 8 );
+			mat.push_back('0' + ( i / 8 ));
 	}
 	
 	void randomSort( int movements )
@@ -42,7 +44,7 @@ struct Cube
 	{
 		for( int i = 0; i < 3; ++ i )
 		{
-			int tmp = mat[ ROTATIONS[idx][i] ];
+			char tmp = mat[ ROTATIONS[idx][i] ];
 			for( int j = 1; j < 4; ++ j )
 				mat[ ROTATIONS[idx][ ( j - 1 ) * 3 + i ] ] = mat[ ROTATIONS[idx][ j * 3 + i ] ];
 			mat[ ROTATIONS[idx][9 + i] ] = tmp;
@@ -52,40 +54,63 @@ struct Cube
 	void print()
 	{
 		puts("");
-		printf("      %d %d %d           \n", mat[8], mat[9], mat[10] );
-		printf("      %d 1 %d           \n", mat[11], mat[12] );
-		printf("      %d %d %d           \n", mat[13], mat[14], mat[15] );
+		printf("      %c %c %c           \n", mat[8], mat[9], mat[10] );
+		printf("      %c 1 %c           \n", mat[11], mat[12] );
+		printf("      %c %c %c           \n", mat[13], mat[14], mat[15] );
 
-		printf("%d %d %d %d %d %d %d %d %d %d %d %d\n", mat[0], mat[1], mat[2], mat[16], mat[17], mat[18], mat[32], mat[33], mat[34], mat[40], mat[41], mat[42] );
-		printf("%d 0 %d %d 2 %d %d 4 %d %d 5 %d\n", mat[3], mat[4], mat[19], mat[20], mat[35], mat[36], mat[43], mat[44] ); 
-		printf("%d %d %d %d %d %d %d %d %d %d %d %d\n", mat[5], mat[6], mat[7], mat[21], mat[22], mat[23], mat[37], mat[38], mat[39], mat[45], mat[46], mat[47] );
+		printf("%c %c %c %c %c %c %c %c %c %c %c %c\n", mat[0], mat[1], mat[2], mat[16], mat[17], mat[18], mat[32], mat[33], mat[34], mat[40], mat[41], mat[42] );
+		printf("%c 0 %c %c 2 %c %c 4 %c %c 5 %c\n", mat[3], mat[4], mat[19], mat[20], mat[35], mat[36], mat[43], mat[44] ); 
+		printf("%c %c %c %c %c %c %c %c %c %c %c %c\n", mat[5], mat[6], mat[7], mat[21], mat[22], mat[23], mat[37], mat[38], mat[39], mat[45], mat[46], mat[47] );
 		
-		printf("      %d %d %d           \n", mat[24], mat[25], mat[26] );
-		printf("      %d 3 %d           \n", mat[27], mat[28] );
-		printf("      %d %d %d           \n", mat[29], mat[30], mat[31] );
+		printf("      %c %c %c           \n", mat[24], mat[25], mat[26] );
+		printf("      %c 3 %c           \n", mat[27], mat[28] );
+		printf("      %c %c %c           \n", mat[29], mat[30], mat[31] );
 		puts("");
 	}
 	
 	bool done()
 	{
 		for( int i = 0; i < STICKERS; ++ i )
-			if( mat[i] != ( i / 8 ) ) 
+			if( mat[i] != '0' + ( i / 8 ) ) 
 				return false;
 		return true;
 	}
 };
 
-int cn = 0;
-bool dfs( Cube * c, int depth = 0 )
+map< string, short > M0;
+
+void dfs0( Cube * c, int depth = 0, int lastMovement = -1 )
 {
-	if( c->done() ) return true;
-	//c->print();
-	++cn;
-	if( depth == 15 ) return false;
+	if( M0.count( c->mat ) && M0[ c->mat ] <= depth ) return;
+	M0[ c->mat ] = depth;
+
+	if( depth == 6 ) return;
+
 	for( int i = 0; i < TOTAL_ROTATIONS; ++ i )
 	{
+		if( i == lastMovement ) continue;
 		c -> rotate( i );
-		if( dfs( c, depth + 1 ) ) return true;
+		dfs0( c, depth + 1, i ^ 1 );
+		c -> rotate( i ^ 1 );
+	}
+}
+
+
+int cn = 0;
+short dfs( Cube * c, int depth = 0, int lastMovement = -1 )
+{
+	if( M0.count( c -> mat ) ) 
+		return depth + M0[ c -> mat] ;
+	//c->print();
+	++cn;
+	if( depth == 9 ) return false;
+	for( int i = 0; i < TOTAL_ROTATIONS; ++ i )
+	{
+		if( i == lastMovement ) continue;
+		c -> rotate( i );
+		short r = dfs( c, depth + 1, i ^ 1 );
+		if( r ) return r; 
+			
 		c -> rotate( i ^ 1 );
 	}
 	return false;
@@ -95,12 +120,20 @@ int main()
 {
 	srand( time ( NULL ) );
 	
-	Cube * c = new Cube();
-	c -> randomSort(15);
-	c -> print();
+	Cube * fc = new Cube();
+	puts("Loading ...");
+	dfs0( fc );
 	
-	dfs( c );
-	c->print();
-	printf("%d\n", cn );
+	for( int it = 0; it < 30; ++ it )
+	{
+		Cube * c = new Cube();
+		puts("Cube generation ...");
+		c -> randomSort(15);
+		c -> print();
+		
+		puts("Solving ...");
+		printf("Solved in %d movements\n", dfs( c ));
+	}
+	cout << 1.0 * clock() / CLOCKS_PER_SEC << endl;
 	return 0;
 }
